aa8e731095e4585ae4cd59a8d2ba2151f29a2ba2
Shooshpanius <admin@in-da-house.ru>
2026-04-08 22:53:22 +0300

    Delete ClubTableTracker.avalonia directory

--- ClubTableTracker.avalonia/ClubTableTracker.avalonia/Services/ApiConfig.cs
+++
@@ -1,8 +0,0 @@
-namespace ClubTableTracker.avalonia.Services;
-
-public static class ApiConfig
-{
-    // All requests go through the nginx reverse proxy at club.wh40kcards.ru
-    // nginx routes /api/* to the backend container (back40club:8080)
-    public const string BaseUrl = "https://club.wh40kcards.ru/api";
-}
