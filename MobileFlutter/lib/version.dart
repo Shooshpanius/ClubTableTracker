9cdfc973ff5947d719fb5f01b1718e2dbfb72fe8
Shooshpanius <admin@in-da-house.ru>
2026-05-18 23:19:49 +0300

    Сборка мобильного приложения локально

--- MobileFlutter/lib/version.dart
+++ MobileFlutter/lib/version.dart
@@ -1,6 +1,14 @@
 /// Версия сборки приложения.
 /// Значения подставляются CI из clubtabletracker.client/src/version.ts
 /// в шаге «Синхронизация версии из браузерной части».
+/// При локальной сборке используются значения по умолчанию.
 
-const String appBuildNumber = 'BUILD_NUMBER';
-const String appBuildDate = 'BUILD_DATE';
+const String _rawBuildNumber = 'BUILD_NUMBER';
+const String _rawBuildDate = 'BUILD_DATE';
+
+String get appBuildNumber =>
+    _rawBuildNumber == 'BUILD_NUMBER' ? 'local' : _rawBuildNumber;
+
+String get appBuildDate => _rawBuildDate == 'BUILD_DATE'
+    ? DateTime.now().toLocal().toString().substring(0, 10)
+    : _rawBuildDate;
