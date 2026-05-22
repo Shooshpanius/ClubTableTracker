063b9e0e86aff5c6f8dbca1bfa20a0fdbb4f8b77
Shooshpanius <admin@in-da-house.ru>
2026-05-22 16:53:09 +0300

    mobile_build_number.txt Add

--- MobileFlutter/lib/version.dart
+++ MobileFlutter/lib/version.dart
@@ -1,13 +1,23 @@
 /// Версия сборки приложения.
-/// Значения подставляются CI из clubtabletracker.client/src/version.ts
-/// в шаге «Синхронизация версии из браузерной части».
+/// Значения подставляются CI в шаге «Обновление version.dart».
+/// Формат полной версии: {WEB_PR_NUMBER}.{MOBILE_BUILD}
 /// При локальной сборке используются значения по умолчанию.
 
+/// Номер последнего веб-PR (берётся из clubtabletracker.client/src/version.ts)
+const String _rawWebVersion = 'WEB_VERSION';
+
+/// Порядковый номер мобильного билда (из MobileFlutter/mobile_build_number.txt)
 const String _rawBuildNumber = 'BUILD_NUMBER';
 const String _rawBuildDate = 'BUILD_DATE';
 
+String get appWebVersion =>
+    _rawWebVersion == 'WEB_VERSION' ? '0' : _rawWebVersion;
+
 String get appBuildNumber =>
-    _rawBuildNumber == 'BUILD_NUMBER' ? 'local' : _rawBuildNumber;
+    _rawBuildNumber == 'BUILD_NUMBER' ? '0' : _rawBuildNumber;
+
+/// Полная версия вида «245.3» (веб-PR . мобильный билд)
+String get appVersion => '$appWebVersion.$appBuildNumber';
 
 String get appBuildDate => _rawBuildDate == 'BUILD_DATE'
     ? DateTime.now().toLocal().toString().substring(0, 10)
