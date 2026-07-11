0ea61e86a1eb37dd17c5613e1938b808b26ba003
Shooshpanius <admin@in-da-house.ru>
2026-07-11 16:22:53 +0300

    LAST_PR_NUMBER = 250

--- clubtabletracker.client/src/version.ts
+++ clubtabletracker.client/src/version.ts
@@ -1,3 +1,3 @@
 // Номер и дата последнего принятого пулл реквеста
-export const LAST_PR_NUMBER = 249;
-export const LAST_PR_DATE = '02.06.2026';
+export const LAST_PR_NUMBER = 250;
+export const LAST_PR_DATE = '11.07.2026';
